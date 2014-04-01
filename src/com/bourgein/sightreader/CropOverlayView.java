50623e6cae36a121f4c041d304c7b57e7d6622af
jemble <jeremy.bourgein@gmail.com>
2014-04-01 17:24:27 +0100

    added cropping gui

--- src/com/bourgein/sightreader/CropOverlayView.java
+++ src/com/bourgein/sightreader/CropOverlayView.java
@@ -2,18 +2,59 @@ package com.bourgein.sightreader;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.ImageView;
 
 public class CropOverlayView extends ImageView {
+	
+	private static final int HANDLE_SIZE = 30;
+	public float x1,x2,y1,y2;
+	public boolean isDrawingCropBox;
+	public RectF topLeftHandle;
+	public RectF bottomRightHandle;
+	private Paint paint;
+	private Paint handlePaint;
 
 	public CropOverlayView(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		
+		isDrawingCropBox = false;
+		x1 = 10;
+		x2 = 110;
+		y1 = 50;
+		y2 = 220;
+		paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		paint.setColor(Color.rgb(246,136,31));
+		paint.setStrokeWidth(2);
+		paint.setStyle(Paint.Style.STROKE);
+		handlePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		handlePaint.setColor(Color.rgb(246,136,31));		
+		topLeftHandle = new RectF();
+		bottomRightHandle = new RectF();
 	}
 	
+	@Override
 	protected void onDraw(Canvas canvas){
 		super.onDraw(canvas);
+		Log.i("JEM","crop pos: "+x1+" "+y1+" "+x2+" "+y2);
+		if(isDrawingCropBox){
+			canvas.drawRect(x1,y1,x2,y2, paint);
+			topLeftHandle.set(x1-HANDLE_SIZE,y1-HANDLE_SIZE,x1+HANDLE_SIZE,y1+HANDLE_SIZE);
+			bottomRightHandle.set(x2-HANDLE_SIZE,y2-HANDLE_SIZE,x2+HANDLE_SIZE,y2+HANDLE_SIZE);
+			canvas.drawRect(topLeftHandle,handlePaint);
+			canvas.drawRect(bottomRightHandle,handlePaint);
+		}
+	}
+	
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec){
+		int parentWidth = MeasureSpec.getSize(widthMeasureSpec);
+	    int parentHeight = MeasureSpec.getSize(heightMeasureSpec);
+	    this.setMeasuredDimension(parentWidth, parentHeight);
+	    super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 	}
 
 }
--- src/com/bourgein/sightreader/FullPhotoPreviewActivity.java
+++ src/com/bourgein/sightreader/FullPhotoPreviewActivity.java
@@ -6,12 +6,15 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class FullPhotoPreviewActivity extends Activity {
 
 	private CropOverlayView cropOverlay;
 	private Song song;
+	private Button btnCropOrange, btnCropGreen, btnStop, btnMusic, btnTrash; 
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +24,58 @@ public class FullPhotoPreviewActivity extends Activity {
 		cropOverlay = (CropOverlayView)findViewById(R.id.fullphoto_crop_photo_preview);
 		Bundle bundle = getIntent().getExtras();
 		song = bundle.getParcelable(SetSongDetailsActivity.SONG_PARCEL);
-		Log.i("JEM","song path: "+song.getImageFileName());
+		
+		btnCropOrange = (Button)findViewById(R.id.fullphoto_btn_crop_orange);
+		btnCropGreen = (Button)findViewById(R.id.fullphoto_btn_crop_green);
+		btnStop = (Button)findViewById(R.id.fullphoto_btn_stop);
+		btnMusic = (Button)findViewById(R.id.fullphoto_btn_music);
+		btnTrash = (Button)findViewById(R.id.fullphoto_btn_trash);
+		
+		btnCropGreen.setVisibility(View.INVISIBLE);
+		btnStop.setVisibility(View.INVISIBLE);
+		
 		Bitmap origBmp = BitmapFactory.decodeFile(song.getImageFileName());
 		cropOverlay.setAdjustViewBounds(true);
 		cropOverlay.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
 		cropOverlay.setImageBitmap(origBmp);
 	}
 
+	public void startCropping(View view){
+		btnCropOrange.setVisibility(View.INVISIBLE);
+		btnMusic.setVisibility(View.INVISIBLE);
+		btnTrash.setVisibility(View.INVISIBLE);
+		
+
+		
+		btnCropGreen.setVisibility(View.VISIBLE);
+		btnStop.setVisibility(View.VISIBLE);
+		
+		//these are here so that we calculate the picView height once we know the image has finished inflating 
+		int cropOverlayHeight = cropOverlay.getHeight();
+		int cropOverlayWidth = cropOverlay.getWidth();
+		
+		cropOverlay.x1 = cropOverlayWidth/4;
+		cropOverlay.y1 = cropOverlayHeight/4;
+		cropOverlay.x2 = (cropOverlayWidth-(cropOverlayWidth/4));
+		cropOverlay.y2 = (cropOverlayHeight-(cropOverlayHeight/4));
+		
+		cropOverlay.invalidate(); //needed so that onDraw gets called again
+		cropOverlay.isDrawingCropBox = true;
+
+	}
+	
+	public void stopCropping(View view){
+		btnCropOrange.setVisibility(View.VISIBLE);
+		btnMusic.setVisibility(View.VISIBLE);
+		btnTrash.setVisibility(View.VISIBLE);
+		
+		btnCropGreen.setVisibility(View.INVISIBLE);
+		btnStop.setVisibility(View.INVISIBLE);
+		
+		cropOverlay.invalidate(); //needed so that onDraw gets called again
+		cropOverlay.isDrawingCropBox = false;
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
