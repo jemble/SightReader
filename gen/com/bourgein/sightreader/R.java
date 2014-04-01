d0aefd7d0ce39da65df24e5c1dca4e5d75317fc6
jemble <jeremy.bourgein@gmail.com>
2014-04-01 12:11:11 +0100

    added Home Activity

--- gen/com/bourgein/sightreader/R.java
+++ gen/com/bourgein/sightreader/R.java
@@ -21,10 +21,31 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int button_camera=0x7f020000;
+        public static final int button_camera_down=0x7f020001;
+        public static final int button_crop_green=0x7f020002;
+        public static final int button_crop_green_down=0x7f020003;
+        public static final int button_crop_orange=0x7f020004;
+        public static final int button_crop_orange_down=0x7f020005;
+        public static final int button_music=0x7f020006;
+        public static final int button_music_down=0x7f020007;
+        public static final int button_play=0x7f020008;
+        public static final int button_play_down=0x7f020009;
+        public static final int button_start=0x7f02000a;
+        public static final int button_start_down=0x7f02000b;
+        public static final int button_start_layout=0x7f02000c;
+        public static final int button_stop=0x7f02000d;
+        public static final int button_stop_down=0x7f02000e;
+        public static final int button_trash=0x7f02000f;
+        public static final int button_trash_down=0x7f020010;
+        public static final int gradient_background=0x7f020011;
+        public static final int ic_launcher=0x7f020012;
+        public static final int logo=0x7f020013;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080002;
+        public static final int btn_start=0x7f080001;
+        public static final int img_logo=0x7f080000;
     }
     public static final class layout {
         public static final int activity_home=0x7f030000;
@@ -35,7 +56,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int start=0x7f050002;
     }
     public static final class style {
         /** 
--- src/com/bourgein/sightreader/HomeActivity.java
+++ src/com/bourgein/sightreader/HomeActivity.java
@@ -2,16 +2,34 @@ package com.bourgein.sightreader;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends Activity {
+	
+	Button btnStart;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+		
+		btnStart = (Button)findViewById(R.id.btn_start);
 	}
 
+	/**
+	 * Handles start button clicked
+	 * Starts the SetSongDetailsActivity
+	 * @param view
+	 */
+	public void btnStartClicked(View view){
+		Intent songDetailsIntent = new Intent(getApplicationContext(),SetSongDetailsActivity.class);
+		startActivity(songDetailsIntent);
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
